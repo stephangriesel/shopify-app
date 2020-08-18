98546fb015fe83486b13f8d3e82888a01ed0d067
Stephan <sgriesel@gmail.com>
2020-08-18 07:39:13 +0200

    add polaris & set up emptystate

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "22651fb915a50a5d306a";
+/******/ 	var hotCurrentHash = "4ef1d6e64e9afac5e599";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-const Index = () => (
-    <div>
-        <p>Test</p>
-    </div>
-)
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+
+// const { render } = require("react-dom")
+
+function Index() {
+    return (
+        <Page>
+            <Layout>
+                <EmptyState
+                    heading="Manage your products"
+                    action={{ content: 'Select Products' }}
+                // secondaryAction={{ content: 'Learn more', url: 'https://help.shopify.com' }}
+                // image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+                >
+                    <p>Select Products</p>
+                </EmptyState>
+            </Layout>
+        </Page>
+    )
+}
 
 export default Index;
