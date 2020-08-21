e2cd84010a8ac8401985cd68efed3ef598df31be
Stephan <sgriesel@gmail.com>
2020-08-21 06:24:46 +0200

    modal logic

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5bab27776645bf71dcf1";
+/******/ 	var hotCurrentHash = "5595f808b2fa6df0172b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,28 @@
+import React, { useState } from 'react';
 import { EmptyState, Layout, Page } from '@shopify/polaris';
+import { ResourcePicker } from '@shopify/app-bridge-react'
 
 // const { render } = require("react-dom")
 
 function Index() {
+
+    const [modal, setModal] = useState({ open: false })
+
     return (
         <Page>
+            <ResourcePicker
+                resourceType="Product"
+                showVariants={false}
+                open={modal.open}
+                onCancel={() => setModal({ open: false })}
+            />
             <Layout>
                 <EmptyState
                     heading="Manage your products"
-                    action={{ content: 'Select Products' }}
+                    action={{
+                        content: 'Select Products',
+                        onAction: () => setModal({ open: true })
+                    }}
                 // secondaryAction={{ content: 'Learn more', url: 'https://help.shopify.com' }}
                 // image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
                 >
