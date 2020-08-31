45ca4eb4cfed671e6fb32f9783ba0ac1247f9646
Stephan <sgriesel@gmail.com>
2020-08-31 06:54:49 +0200

    test script tag - success

--- public/test-script.js
+++ public/test-script.js
@@ -1,1 +1,5 @@
 console.log("**script tag api test**")
+
+const header = $('header.site-header').parent();
+
+header.prepend('<div>header test</div>').css({ 'background-color': '#000', 'text-align': 'center' })
